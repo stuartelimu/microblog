45abecdf3a3d044381a5aefbc8e45078ebd26651
stuartelimu <stuartelimu@gmail.com>
2021-02-23 19:37:23 +0300

    refactor code for proper urls

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -25,6 +25,6 @@ def login():
         flash('login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
     
