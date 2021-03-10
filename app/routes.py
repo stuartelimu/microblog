f0a85e1ce5451274792e8b5fafcfd6356b81335f
stuartelimu <stuartelimu@gmail.com>
2021-03-10 04:40:53 +0300

    show logged in user in template

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,6 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'stue'}
     posts = [
         {
             'author': {'username': 'Jane'},
@@ -20,7 +19,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         },
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
